6dbd978facbd58c9f8c17bab8004e16cd7ce543b
Anna <nemotim@yandex.ru>
2022-04-14 10:31:09 +0300

    tests fix

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -10,17 +10,16 @@ class DifferTest extends TestCase
 {
     public function testGenDiff()
     {
-        // $expected1 = file_get_contents("tests/fixtures/resultTree.txt");
-        $expected1 = genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json");
+        $expected1 = file_get_contents("tests/fixtures/resultTree.txt");
         $this->assertEquals($expected1, genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json"));
 
-        // $expected2 = file_get_contents("tests/fixtures/result1.txt");
-        // $this->assertEquals($expected2, genDiff("tests/fixtures/firstFile.yaml", "tests/fixtures/secondFile.yaml"));
+        $expected2 = file_get_contents("tests/fixtures/result1.txt");
+        $this->assertEquals($expected2, genDiff("tests/fixtures/firstFile.yaml", "tests/fixtures/secondFile.yaml"));
 
-        // $expected3 = file_get_contents("tests/fixtures/resultPlane.txt");
-        // $this->assertEquals($expected3, genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'plane'));
+        $expected3 = file_get_contents("tests/fixtures/resultPlane.txt");
+        $this->assertEquals($expected3, genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'plane'));
 
-        // $expected4 = file_get_contents("tests/fixtures/resultJson.txt");
-        // $this->assertEquals($expected4, genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'json'));
+        $expected4 = file_get_contents("tests/fixtures/resultJson.txt");
+        $this->assertEquals($expected4, genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'json'));
     }
 }
