22fc20562fad98ba8eecdab7faf4f61f3dd7f090
Anna <nemotim@yandex.ru>
2022-04-14 09:41:17 +0300

    tests fix

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -12,18 +12,18 @@ class DifferTest extends TestCase
     {
         $expected1 = file_get_contents("tests/fixtures/resultTree.txt");
         $actual1 = genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json");
-        $this->assertSame($expected1, "{$actual1}\n");
+        $this->assertEquals($expected1, "{$actual1}\n");
 
         $expected2 = file_get_contents("tests/fixtures/result1.txt");
         $actual2 = genDiff("tests/fixtures/firstFile.yaml", "tests/fixtures/secondFile.yaml");
-        $this->assertSame($expected2, "{$actual2}\n");
+        $this->assertEquals($expected2, "{$actual2}\n");
 
         $expected3 = file_get_contents("tests/fixtures/resultPlane.txt");
         $actual3 = genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'plain');
-        $this->assertSame($expected3, "{$actual3}\n");
+        $this->assertEquals($expected3, "{$actual3}\n");
 
         $expected4 = file_get_contents("tests/fixtures/resultJson.txt");
         $actual4 = genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'json');
-        $this->assertSame($expected4, "{$actual4}\n");
+        $this->assertEquals($expected4, "{$actual4}\n");
     }
 }
