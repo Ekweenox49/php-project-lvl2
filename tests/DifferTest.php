53be136910e88d05898dc179014b43733257f770
Anna <nemotim@yandex.ru>
2022-04-14 10:13:25 +0300

    tests fix

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -3,27 +3,22 @@
 namespace Hexlet\Phpunit\Tests;
 
 use PHPUnit\Framework\TestCase;
-
 use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
     public function testGenDiff()
     {
-        $expected1 = file_get_contents("tests/fixtures/resultTree.txt");
-        $actual1 = genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json");
-        $this->assertEquals($expected1, "{$actual1}\n");
+        $expected1 = (string) file_get_contents("tests/fixtures/resultTree.txt");
+        $this->assertEquals($expected1, genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json"));
 
-        $expected2 = file_get_contents("tests/fixtures/result1.txt");
-        $actual2 = genDiff("tests/fixtures/firstFile.yaml", "tests/fixtures/secondFile.yaml");
-        $this->assertEquals($expected2, "{$actual2}\n");
+        $expected2 = (string) file_get_contents("tests/fixtures/result1.txt");
+        $this->assertEquals($expected2, genDiff("tests/fixtures/firstFile.yaml", "tests/fixtures/secondFile.yaml"));
 
-        $expected3 = file_get_contents("tests/fixtures/resultPlane.txt");
-        $actual3 = genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'plain');
-        $this->assertEquals($expected3, "{$actual3}\n");
+        $expected3 = (string) file_get_contents("tests/fixtures/resultPlane.txt");
+        $this->assertEquals($expected3, genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'plane'));
 
-        $expected4 = file_get_contents("tests/fixtures/resultJson.txt");
-        $actual4 = genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'json');
-        $this->assertEquals($expected4, "{$actual4}\n");
+        $expected4 = (string) file_get_contents("tests/fixtures/resultJson.txt");
+        $this->assertEquals($expected4, genDiff("tests/fixtures/firstTree.json", "tests/fixtures/secondTree.json", 'json'));
     }
 }
